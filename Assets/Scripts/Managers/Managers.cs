c3dad56f2d8b718a0d946fbbb9b2de495312d272
dydtjrsj21 <44419206+dydtjrsj21@users.noreply.github.com>
2022-04-13 00:24:07 +0900

    Input System Completed
    0. 물체 생성
    -프리팹 만들기
    
    1. 조작방법
    - 마우스와 wasd 1인칭 카메라로 시점 조절
    - x키 누르면 가운데 빔 쏘고 맞은 오브젝트 선택 및 선택해제 가능
    - 선택되면 색깔 및 투명도 변경

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; // 유일성이 보장된다
+    static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
+
+    InputManager _input = new InputManager();
+    PoolManager _pool = new PoolManager();
+    ResourceManager _resource = new ResourceManager();
+    SceneManagerEx _scene = new SceneManagerEx();
+    SoundManager _sound = new SoundManager();
+    UIManager _ui = new UIManager();
+
+    public static InputManager Input { get { return Instance._input; } }
+    public static PoolManager Pool { get { return Instance._pool; } }
+    public static ResourceManager Resource { get { return Instance._resource; } }
+    public static SceneManagerEx Scene { get { return Instance._scene; } }
+    public static SoundManager Sound { get { return Instance._sound; } }
+    public static UIManager UI { get { return Instance._ui; } }
+
+    void Start()
+    {
+        s_instance = this;
+        _input.Clear();
+        string[] objects = Enum.GetNames(typeof(Define.Objects));
+        foreach(string obj in objects)
+        {
+            GameObject parent = new GameObject(obj+"s");
+            for(int i = 0; i < 10; i++)
+            {
+                GameObject child = _resource.Instantiate(obj, parent.transform);
+                child.transform.position = new Vector3(UnityEngine.Random.Range(-5, 5), 
+                     0.0f, UnityEngine.Random.Range(-5, 5));
+            }
+
+        }
+             
+    }
+
+    void Update()
+    {
+        _input.OnUpdate();
+    }
+
+    static void Init()
+    {	
+	}
+
+    public static void Clear()
+    {
+        //Input.Clear();
+        //Sound.Clear();
+        //Scene.Clear();
+        //UI.Clear();
+        //Pool.Clear();
+    }
+}
---
+++ Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum Scene
+    {
+        Unknown,
+        Login,
+        Lobby,
+        Game,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,
+    }
+
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+        Move
+    }
+
+    public enum CameraMode
+    {
+        QuarterView,
+    }
+
+    public enum Objects {
+        Cube
+    }
+}
