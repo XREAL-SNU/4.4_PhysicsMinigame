c684e448655ef0d4b7c5b1aba8dd9d45dfddf8a6
kinajin <kinajin22@gmail.com>
2022-04-14 02:01:43 +0900

    [feature] add endpoint

--- Assets/Script/Coins.cs
+++ Assets/Script/Coins.cs
@@ -10,6 +10,12 @@ public class Coins : MonoBehaviour
     public float rotateSpeed;
 
 
+
+    void Awake()
+    {
+    }
+
+
     void Update()
     {
         transform.Rotate(Vector3.up  * rotateSpeed * Time.deltaTime );
@@ -17,30 +23,5 @@ public class Coins : MonoBehaviour
     }
 
 
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Hodu") {
-
-
-            Hodu hodu = other.GetComponent<Hodu>();
-            // 여기서 Hodu는 스크립트 이름 
-
-
-
-            hodu.itemCount++; 
-
-
-
-            gameObject.SetActive(false);
-            // SetActive(bool): 오브젝트 활성화 비활성, 여기서는 비활성화
-
-
-
-
-
-        }
     }
 
-
-}
--- Assets/Script/Hodu.cs
+++ Assets/Script/Hodu.cs
@@ -1,29 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+//SceneManager 장면을 관리하는 기본 클래스 
 
 public class Hodu : MonoBehaviour
 {
 
 
     public float jumpPower;
-    public int itemCount;
+    public int bitcoinItemCount;
+    public GameManagerLogic manager;
     bool isJump;
     Rigidbody rigid;
+    AudioSource audio;
+
+
+
 
     void Awake()
     {
-
         isJump = false;
         rigid = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump") && !isJump ) {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
             isJump = true;
             rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
-
         }
 
     }
@@ -45,13 +52,73 @@ public class Hodu : MonoBehaviour
     {
         if (collision.gameObject.name == "Floor")
             isJump = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+
+        //비트코인 카운트 추가
+
+        if (other.name == "Bitcoin")
+        {
+            bitcoinItemCount++;
+        }
+
+
+
+        // 만약에 코인을 먹으면 소리 나오	
+
+        if (other.tag == "Coins")
+        {
+            audio.Play();
+            //플레이어가 아이템을 먹고나면 오디오 제생
+
+            other.gameObject.SetActive(false);
+            // SetActive(bool): 오브젝트 활성화 비활성, 여기서는 비활성화
+
+        }
+
+        else if (other.name == "Finish Point") {
+
+
+            if (bitcoinItemCount == manager.TotalItemCount)
+            {
+
+                SceneManager.LoadScene("Sucess");
+                //game clear
+
+            }
+
+
+            else {
+
+                // restart
+                SceneManager.LoadScene("Fail");
+
+            }
+
+
+
+            //Find함수는 CPU 함수 부하를 초래할 수 있어서 피하는 것을 권장 
+
+
+
+
+
+        }
+
+
 
 
 
 
 
     }
-
-
-
 }
+
+
+
+
