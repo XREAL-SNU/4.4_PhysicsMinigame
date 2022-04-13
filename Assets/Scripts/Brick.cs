0e71ae84202b287e25b9aa6314feb2a4126bd817
p4r4dox82 <81799661+p4r4dox82@users.noreply.github.com>
2022-04-14 05:12:27 +0900

    [feat] Material and Game Start
    메터리얼 추가하고, 게임 시작부분 구현

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Brick : Interface
 {
     private bool isEnoughHeight = false;
+    private bool isInterface = false;
+    private int clickedNum = 0;
 
     public override void Start() {
         GameManager.Instance().addInterface("Brick", this.GetComponent<Brick>());
@@ -14,8 +17,22 @@ public class Brick : Interface
         
     }
 
+    public void OnMouseDown() {
+        clickedNum += 1;
+        if(clickedNum == 1) {
+            DOTween.Sequence()
+            .Append(transform.DORotate(new Vector3(0f, 0f, 10f), 0.5f))
+            .Insert(0f, transform.DOLocalMove(new Vector3(7.7f, 10f, -25f), 0.5f));
+        } if (clickedNum >= 2) {
+            Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+            rigidbody.useGravity = true;
+        }
+    }
+
     public override void OnMouseDrag() {
-        base.OnMouseDrag();
+        if(clickedNum > 2) {
+            base.OnMouseDrag();
+        }
         if(transform.position.y > 6) {
             isEnoughHeight = true;
         }
