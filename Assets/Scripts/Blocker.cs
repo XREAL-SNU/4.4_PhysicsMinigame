36192a8b5db7639e723efddbfa05e0e6b7203e89
uni20201266 <SeeWhy3323@gmail.com>
2022-04-25 14:42:32 +0900

    implemented basic functions

--- Assets/Scripts/Blocker.cs
+++ Assets/Scripts/Blocker.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Blocker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private int _level;
+    private float _firespeed;
+    private Vector3 _location;
+    private int _hp;
+    public void init(Vector3 locate, int lev){
+        _location = locate;
+        transform.position += locate;
+        _level = lev;
+        _firespeed = setFireSpeed(lev);
+        _hp = setHp(lev);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void levelchange(int delta){
+        _level += delta;
+        _firespeed = setFireSpeed(_level);
+        _hp = setHp(_level);
     }
+    public int getLevel(){
+        return _level;
+    }
+    private float setFireSpeed(int lev){
+        if(lev == 0)
+            return 0;
+        return 3.0f / lev;
+    }
+    private int setHp(int lev){
+        return 100 * (lev+1);
+    }
+    public int hit(int damage){
+        _hp -= damage;
+        return _hp;
+    }
+    
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _round;
+    private int _gold;
+    // If true, the game is over.
+    private bool _GameOver;
+    private bool _InGame;
+
+    void Awake()
     {
-        
+        init();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void init(){
+        _round = 0;
+        _gold = 100;
+        _GameOver = false;
+        _InGame = false;
+    }
+
+    public int getRound(){
+        if(!_GameOver)
+            return _round;
+        return -1;
+    }
+    public void increaseRound(){
+        if(!_GameOver)
+            _round += 1;
+    }
+    public int getGold(){
+        if(!_GameOver)
+            return _gold;
+        return -1;
+    }
+    public bool changeGold(int change){
+        _gold += change;
+        if(g_old < 0){
+            _gold -= change;
+            return false;
+        }
+        return true;
+    }
+    public bool getGameOver(){
+        return _GameOver;
+    }
+    public void setGameOver(){
+        _GameOver = true;
+    }
+    public bool getInGame(){
+        return _InGame;
+    }
+    public void setInGame(bool set){
+        _InGame = set;
     }
 }
